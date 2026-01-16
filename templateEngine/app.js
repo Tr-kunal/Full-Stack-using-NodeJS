f18b648dcf6177a643fb9ac07298d253465f9db6
Kunal <kunalsinghal365@gmail.com>
2026-01-16 18:37:37 +0530

    Set up initial Express application with EJS views, static files, and routes

--- templateEngine/app.js
+++ templateEngine/app.js
@@ -5,13 +5,18 @@ const port = 4000;
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',(req,res)=>{
     res.send("working fine")
 })
 
-
 const loc = path.join(__dirname,'index.html');
+
+app.get('/home',(req,res)=>{
+    res.render("home")
+})
+
 app.get('/about',(req,res)=>{
     res.sendFile(loc)
 })
@@ -20,15 +25,11 @@ app.get('/profile',(req,res)=>{
     res.render("profile")
 })
 
-app.get('/payment',(req,res)=>{
-    res.render("abc")
+app.get('/contacts',(req,res)=>{
+    res.render("contacts")
 })
 
-const todo = ['coding','sleeping','eating'];
 
-app.get('/todo',(req,res)=>{
-    res.render("todo",{todo})
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
